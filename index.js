288706198c9b64048e869a4d5523b2894519e986
bhfreeman <brett.h.freeman@gmail.com>
2021-03-17 18:27:51 -0700

    added questions array, linked js files

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs")
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter the description of the project:'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter the projectinstallation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter the usage information of the project:'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: "What are the contribution guidelines for this project?"
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: "What are the testing instructions for the project?"
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "What license does the project fall under?",
+        choices: ["GNU AGPLv3","GNU GPLv3","GNU LGPLv3","Mozilla Public License 2.0","Apache License 2.0","MIT License","Boost Software License 1.0","The Unlicense"]
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter the github username associated with this project:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter the email associated with this project:'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md',data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => console.log(data));
+}
 
 // Function call to initialize app
 init();
